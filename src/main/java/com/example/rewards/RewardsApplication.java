11392c1a7f7e390c4b694e2bc48dd814d736e1af
Admin <admin@Admins-MacBook-Pro.local>
2024-07-22 12:46:14 +0530

    logger added

--- src/main/java/com/example/rewards/RewardsApplication.java
+++ src/main/java/com/example/rewards/RewardsApplication.java
@@ -1,14 +1,19 @@
 package com.example.rewards;
 
+import java.util.logging.Logger;
+
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RewardsApplication {
 
+		    private static final Logger logger = (Logger) LoggerFactory.getLogger(RewardsApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(RewardsApplication.class, args);
-		System.out.println("welcome to rewards application using sprintboots");
+		logger.info("welcome to rewards application using sprintboots");
 
 	}
 
